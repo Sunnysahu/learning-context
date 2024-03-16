a045c138711e7db50f9f130c7c99a7162d699cc7
Sunny Sahu <sunnysahu8210@gmail.com>
2024-03-16 22:36:36 +0530

    Context Working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import "./App.css";
 import UserContextProvider from "./context/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +11,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>Sunny Sahu</h1>
+      <Login></Login>
+      <Profile></Profile>
     </UserContextProvider>
   );
 }
