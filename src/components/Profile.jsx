90c72f16e29e9ff7db55669b5aa445c71d7c79aa
Sunny Sahu <sunnysahu8210@gmail.com>
2024-03-16 23:11:40 +0530

    Added Comments

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -14,3 +14,17 @@ function Profile() {
 }
 
 export default Profile;
+
+/*
+
+How it flows
+
+1. Createed a USerContext.JS File and Created used React.createContext() and Exported it
+2. Made a USerCOntextProvider.JSX file and Made a variable inside that, create whatever data you want and pass it as PROPS with UserContext.Provider ( EX -> <UserContext.Provider value={{ user, setUser }}> ) and Export the UserContextProvider.
+3. Now in App.JSX File Wrap the Components you want the flow of data <UserContextProvider> <UserContextProvider/>
+
+to Use the Context
+
+--> Got to Whatever Component and Extract the PROPS using **Data Destructuring** with useContext(UserContext) and Pass the UserContext, which made at very 1st
+
+*/
