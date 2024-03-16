ac05f215aa6d491139f98bf97c87e6fc3955788f
Sunny Sahu <sunnysahu8210@gmail.com>
2024-03-16 22:48:58 +0530

    Project Done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,12 @@ import UserContext from "../context/UserContext";
 function Profile() {
   const { user } = useContext(UserContext);
   if (!user) return <div>Please Login</div>;
-  return <div>Welcome {user.username}</div>;
+  return (
+    <>
+      <div>Welcome {user.username}</div>
+      <div>Your Password is {user.password}</div>
+    </>
+  );
 }
 
 export default Profile;
